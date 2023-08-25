38246894b4c2fcf3808f0e126b579ea4ab8dd841
Kent Sapp <66083310+cksapp@users.noreply.github.com>
2023-08-25 14:29:52 -0400

    Convert secure string
    Convert secure strong to plain text string to pass in X-app-Apikey header

--- Refresh.ps1
+++ Refresh.ps1
@@ -1,12 +1,12 @@
-## Set the URL of the API you want to access
+# Set the URL of the API you want to access
 $url = "https://dbpool.datto.net/api/v2/containers"
 
-## Set the path to the environment variable file
+# Set the path to the environment variable file
 $envFilePath = "$PSScriptRoot\override.env"
 
-## Check if the Override.env file exists, otherwise ask the user for their API key
+# Check if the Override.env file exists, otherwise ask the user for their API key
 if (Test-Path -Path $envFilePath -PathType Leaf) {
-    ## Read the file and convert it into key-value pairs
+    # Read the file and convert it into key-value pairs
     $envVariables = Get-Content $envFilePath | ForEach-Object {
         $parts = $_ -split '='
         $key = $parts[0]
@@ -17,23 +17,26 @@ if (Test-Path -Path $envFilePath -PathType Leaf) {
         }
     }
 
-    ## Display the environment variables
+    # Display the environment variables
     #foreach ($variable in $envVariables) {
     #    Write-Host "Key: $($variable.Key), Value: $($variable.Value)"
     #}
 } else {
-    $p_apiKey = Read-Host "Please enter your DBPool Personal API Key" -AsSecureString
+    $p_apiKeySecure = Read-Host "Please enter your DBPool Personal API Key" -AsSecureString
 }
 
-## Prepare headers with the API key
+# Convert the secure string to a plain text string
+$p_apiKey = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($p_apiKeySecure))
+
+# Prepare headers with the API key
 $headers = @{
-    "X-App-Apikey" = $($p_apiKey)
+    "X-App-Apikey" = $p_apiKey
 }
 
-## Make an API request with the API key in the headers
+# Make an API request with the API key in the headers
 $apiResponse = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
 #$ids = $apiResponse | Select-Object -ExpandProperty id
 
-## Display the response content
+# Display the response content
 $apiResponse.Content
 #Write-Host "IDs: $ids"

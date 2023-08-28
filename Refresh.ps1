2d689d7dd57153307d6bcd314b0e84d475e12048
Kent Sapp <66083310+cksapp@users.noreply.github.com>
2023-08-28 10:25:08 -0400

    Restructure
    Changed a few line locations and added some comments about sections

--- Refresh.ps1
+++ Refresh.ps1
@@ -1,11 +1,20 @@
-# Sets the Security Protocol for a .NET application to use TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+<#
+    Feel free to change these varaibles as needed for your environment if needed.
+#>
+
+# Set the path to the environment variable file
+$envFilePath = "$PSScriptRoot\override.env"
 
 # Set the URL of the API you want to access
 $url = "https://dbpool.datto.net/api/v2/containers"
 
-# Set the path to the environment variable file
-$envFilePath = "$PSScriptRoot\override.env"
+<#
+    Please do not make any changes below this line unless you know what you are doing.
+    If you would like to suggest a change, Pull Requests are always welcome.
+#>
+
+# Sets the Security Protocol for a .NET application to use TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Check if the Override.env file exists, otherwise ask the user for their API key
 if (Test-Path -Path $envFilePath -PathType Leaf) {

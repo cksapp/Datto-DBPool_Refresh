4fbae8645dc1e5a9f261f14894175d8d9c31168e
Kent Sapp <66083310+cksapp@users.noreply.github.com>
2023-11-07 20:08:28 -0500

    Return variables
    Set override variables in an array and then return those variables being set

--- src/example-env_override.ps1
+++ src/example-env_override.ps1
@@ -1,4 +1,17 @@
 # This is an example override.env file, accepts lines commented out with `#`
-$apiUrl = "https://dbpool.datto.net"
-# $apiKey="your_api_key_here"
-$ids="xxxxx"
+$variables = @{
+    # Container IDs to refresh, by default all containers will be refreshed
+    #ids = @(1, 2, 3)
+
+    # Enter your API key from DBPool
+    #apiKey = "your_api_key_here"
+
+    # URL of the API to be checked, defaulted to "https://dbpool.datto.net" in the script already and should not need to be changed or uncommented.
+    #apiUrl = "https://dbpool.datto.net"
+}
+
+# Loop through the hashtable and create variables with default values
+foreach ($variableName in $variables.Keys) {
+    New-Variable -Name $variableName -Value $variables[$variableName] -Force
+    Write-Verbose -Message "Variable `$$variableName imported."
+}

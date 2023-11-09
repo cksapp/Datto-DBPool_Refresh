e042cad0b95851f5b3706ce6761dd6e34e0ee28b
Kent Sapp <66083310+cksapp@users.noreply.github.com>
2023-11-08 19:54:20 -0500

    Update env override and refresh script

--- src/example-env_override.ps1
+++ src/example-env_override.ps1
@@ -1,17 +1,44 @@
-# This is an example override.env file, accepts lines commented out with `#`
-$variables = @{
-    # Container IDs to refresh, by default all containers will be refreshed
-    #ids = @(1, 2, 3)
+function Import-EnvironmentOverride {
+    <#
+    .SYNOPSIS
+       This function creates variables based on a hashtable of default values to import into a session.
 
-    # Enter your API key from DBPool
-    #apiKey = "your_api_key_here"
+    .DESCRIPTION
+       Change the values in the hashtable defined in $variables to suit your needs.
+       Users can specify the variable scope using the -VariableScope parameter.
 
-    # URL of the API to be checked, defaulted to "https://dbpool.datto.net" in the script already and should not need to be changed or uncommented.
-    #apiUrl = "https://dbpool.datto.net"
-}
+    .PARAMETER VariableScope
+       Specifies the scope in which the variables should be created (default is Global).
 
-# Loop through the hashtable and create variables with default values
-foreach ($variableName in $variables.Keys) {
-    New-Variable -Name $variableName -Value $variables[$variableName] -Force
-    Write-Verbose -Message "Variable `$$variableName imported."
+    .EXAMPLE
+       New-OverrideVariables -VariableScope "Script"
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $False, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+        [ValidateSet("Global", "Script", "Local", "Private", "AllUsers", "CurrentUser")]
+        [String]$VariableScope = "Global"
+    )
+
+    $variables = @{
+        # Container IDs to refresh, by default all containers will be refreshed
+        #ids = @(1, 2, 3)
+
+        # Enter your API key from DBPool
+        #apiKey = "your_api_key_here"
+
+        # Timeout for the script to wait for child process jobs to "complete" and return a response before exiting.
+        # Default in the script is set to 60 seconds, this still appears to send the refresh command but may not wait to provide a response if you need that. 
+        #waitSeconds = 300
+
+        # URL of the API to be checked, defaulted to "https://dbpool.datto.net" in the script already and should not need to be changed or uncommented.
+        #apiUrl = "https://dbpool.datto.net"
+    }
+
+    # Loop through the hashtable and create variables with default values
+    foreach ($variableName in $variables.Keys) {
+        New-Variable -Name $variableName -Value $variables[$variableName] -Force -Scope $VariableScope
+        Write-Verbose -Message "Variable `$$variableName imported."
+    }
 }
